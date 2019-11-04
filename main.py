d9dd92376b56691a3fe33692ff681752ea97bb74
Rayzr522 <rayzr522@gmail.com>
2019-11-04 08:49:43 -0500

    Added proper handling of loss-of-connections with the Discord client

--- main.py
+++ main.py
@@ -14,8 +14,16 @@ manager = Playerctl.PlayerManager()
 
 print("Starting RPC client...")
 RPC = pypresence.Presence('440997014315204609')
-RPC.connect()
-print("RPC client connected")
+
+def connect_rpc():
+    while True:
+        try:
+            RPC.connect()
+            print("RPC client connected")
+            break
+        except ConnectionRefusedError as e:
+            print("Failed to connect to RPC! Trying again in 10 seconds...")
+            time.sleep(10)
 
 def setup_player(name):
     player = Playerctl.Player.new_from_name(name)
@@ -31,13 +39,17 @@ def get_song(player):
 def update(player):
     status = player.get_property('status')
 
-    if status == "":
-        RPC.clear()
-    elif status == "Playing":
-        song = get_song(player)
-        RPC.update(state='Playing', details=song, large_image='music', large_text=song, small_image='play')
-    elif status == "Paused":
-        RPC.update(state='Paused', large_image='music', small_image='pause')
+    try:
+        if status == "":
+            RPC.clear()
+        elif status == "Playing":
+            song = get_song(player)
+            RPC.update(state='Playing', details=song, large_image='music', large_text=song, small_image='play')
+        elif status == "Paused":
+            RPC.update(state='Paused', large_image='music', small_image='pause')
+    except pypresence.exceptions.InvalidID:
+        print("Lost connection to Discord RPC! Attempting reconnection...")
+        connect_rpc()
 
 def on_play(player, status, manager):
     update(player)
@@ -53,7 +65,14 @@ def on_player_add(manager, name):
 
 def on_player_remove(manager, player):
     if len(manager.props.players) < 1:
-        RPC.clear()
+        try:
+            RPC.clear()
+        except pypresence.exceptions.InvalidID:
+            if e is "Client ID is Invalid":
+                print("Lost connection to Discord RPC! Attempting reconnection...")
+                connect_rpc()
+            else:
+                raise
     else:
         update(manager.props.players[0])
 
@@ -63,4 +82,6 @@ manager.connect('player-vanished', on_player_remove)
 for name in manager.props.player_names:
     setup_player(name)
 
+# Start program, connect to Discord & hook into GLib's main loop
+connect_rpc()
 GLib.MainLoop().run()
